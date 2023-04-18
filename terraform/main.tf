699fc2da59b7a5d4858c684e9184b3dbee5c62ee
Александр Прокуда <a.prokuda@mcorp.kz>
2023-04-18 17:23:02 +0600

    changed project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
   user     = "administrator@vsphere.local"
-  password = "********"
-  vsphere_server = "ip-vsphere"
+  password = "******"
+  vsphere_server = "10.33.128.100"
 
   allow_unverified_ssl = true
 }
@@ -26,20 +26,26 @@ data "vsphere_network" "network" {
 }
  
 data "vsphere_host" "host" {
-  name          = "ip-host"
+  name          = "10.33.128.26"
   datacenter_id         = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "Template_CentOS_Stab"
+  name          = "Template-CentOS-SSHKey"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 
-# ========================Deploy VM Apache-1===========================================
+# ======================== Deploy VM Apache ===========================================
 
-resource "vsphere_virtual_machine" "Apache-1-TR" {
-  name             = "Apache-1-TR"
+
+locals {
+  ip_addresses = ["10.33.133.70", "10.33.133.71", "10.33.133.72"]
+}
+
+resource "vsphere_virtual_machine" "apache-tf" {
+  count            = 3
+  name             = "apache-${count.index+1}-tf"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id = data.vsphere_datastore.datastore.id
     
@@ -63,13 +69,13 @@ clone {
 
     customize {
       linux_options {
-        host_name = "apache-1-tr"
+        host_name = "apache-${count.index+1}-tf"
         domain    = "mcorp.kz"
       }
       dns_server_list     = ["10.33.130.11", "10.25.130.11"]
   
       network_interface {
-        ipv4_address = "10.33.133.70" 
+        ipv4_address =  element(local.ip_addresses, count.index) 
         ipv4_netmask = 24
       }
  
@@ -83,129 +89,10 @@ provisioner "remote-exec" {
       "echo network: {config: disabled} >> /etc/cloud/cloud.cfg", 
       ]
      connection {
-     host        = "10.33.133.70"
+     host        = element(local.ip_addresses, count.index)
      type        = "ssh"
      user        = "root"
-     password = "*******"
-     }
-
-   }
-
- }
-
-
-# ========================Deploy VM Apache-2===========================================
-
-resource "vsphere_virtual_machine" "Apache-2-TR" {
-  name             = "Apache-2-TR"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id = data.vsphere_datastore.datastore.id
-    
-  num_cpus = 2
-  memory   = 4096
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
-
-  network_interface {
-    network_id = data.vsphere_network.network.id 
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-    thin_provisioned = false
-  }
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = "apache-2-tr"
-        domain    = "mcorp.kz"
-      }
-      dns_server_list     = ["10.33.130.11", "10.25.130.11"]
-  
-      network_interface {
-        ipv4_address = "10.33.133.71" 
-        ipv4_netmask = 24
-      }
- 
-      ipv4_gateway = "10.33.133.1"
-    }
-
-    
-  }
-
-provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "echo network: {config: disabled} >> /etc/cloud/cloud.cfg", 
-      ]
-     connection {
-     host        = "10.33.133.71"
-     type        = "ssh"
-     user        = "root"
-     password = "*******"
-     }
-
-   }
-
- }
-
-# ========================Deploy VM Apache-3===========================================
-
- resource "vsphere_virtual_machine" "Apache-3-TR" {
-  name             = "Apache-3-TR"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id = data.vsphere_datastore.datastore.id
-    
-  num_cpus = 2
-  memory   = 4096
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
-
-  network_interface {
-    network_id = data.vsphere_network.network.id 
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-    thin_provisioned = false
-  }
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = "apache-3-tr"
-        domain    = "mcorp.kz"
-      }
-      dns_server_list     = ["10.33.130.11", "10.25.130.11"]
-  
-      network_interface {
-        ipv4_address = "10.33.133.72" 
-        ipv4_netmask = 24
-      }
- 
-      ipv4_gateway = "10.33.133.1"
-    }
-
-    
-  }
-
-provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "echo network: {config: disabled} >> /etc/cloud/cloud.cfg", 
-      ]
-     connection {
-     host        = "10.33.133.72"
-     type        = "ssh"
-     user        = "root"
-     password = "*******"
+     private_key = file("~/.ssh/id_rsa")
      }
 
    }
@@ -215,8 +102,8 @@ provisioner "remote-exec" {
  
 # ========================Deploy VM HAProxy===========================================
 
-resource "vsphere_virtual_machine" "HAProxy-TR" {
-  name             = "HAProxy-TR"
+resource "vsphere_virtual_machine" "haproxy-tf" {
+  name             = "haproxy-tf"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id = data.vsphere_datastore.datastore.id
     
@@ -240,7 +127,7 @@ clone {
 
     customize {
       linux_options {
-        host_name = "haproxy-tr"
+        host_name = "haproxy-tf"
         domain    = "mcorp.kz"
       }
       dns_server_list     = ["10.33.130.11", "10.25.130.11"]
@@ -252,7 +139,6 @@ clone {
  
       ipv4_gateway = "10.33.133.1"
     }
-
     
   }
 
@@ -265,7 +151,7 @@ provisioner "remote-exec" {
      host        = "10.33.133.73"
      type        = "ssh"
      user        = "root"
-     password = "*******"
+     private_key = file("~/.ssh/id_rsa")
      }
 
    }
@@ -274,8 +160,8 @@ provisioner "remote-exec" {
 
 # ========================Deploy VM Ansible===========================================
 
- resource "vsphere_virtual_machine" "Ansible-TR" {
-  name             = "Ansible-TR"
+ resource "vsphere_virtual_machine" "ansible-tf" {
+  name             = "ansible-tf"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id = data.vsphere_datastore.datastore.id
     
@@ -299,7 +185,7 @@ clone {
 
     customize {
       linux_options {
-        host_name = "ansible-tr"
+        host_name = "ansible-tf"
         domain    = "mcorp.kz"
       }
       dns_server_list     = ["10.33.130.11", "10.25.130.11"]
@@ -321,19 +207,43 @@ provisioner "remote-exec" {
       "echo network: {config: disabled} >> /etc/cloud/cloud.cfg",
       "yum -y install epel-release",
       "yum -y install ansible",
-      "echo [defaults] > /etc/ansible/ansible.cfg",
-      "echo host_key_checking = False >> /etc/ansible/ansible.cfg",
-      "echo inventory = /etc/ansible/hosts >> /etc/ansible/ansible.cfg", 
       
       ]
      connection {
      host        = "10.33.133.74"
      type        = "ssh"
      user        = "root"
-     password = "*******"
+     private_key = file("~/.ssh/id_rsa")
      }
 
    }
 
- }
+provisioner "file" {
+    source      = "C:/Terraform/project-tr/ansible"
+    destination = "/etc/"
+     connection {
+     host        = "10.33.133.74"
+     type        = "ssh"
+     user        = "root"
+     private_key = file("~/.ssh/id_rsa")
+     }
+
+   }
+
+
+provisioner "remote-exec" {
+    inline = [
+      "ansible-playbook /etc/ansible/playbook-apache.yml",     
+      "ansible-playbook /etc/ansible/playbook-haproxy.yml", 
+      ]
+     connection {
+     host        = "10.33.133.74"
+     type        = "ssh"
+     user        = "root"
+     private_key = file("~/.ssh/id_rsa")
+     }
+
+   }
+
+}
 
